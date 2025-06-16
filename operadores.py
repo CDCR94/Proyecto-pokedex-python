c54c050bc06d4baba61d013d76243340974849dc
CDCR <choridad@hotmail.com>
2025-06-16 09:26:39 -0600

    practicas de python

---
+++ operadores.py
@@ -0,0 +1,39 @@
+#operadores aritmeticos
+# print(2+2)
+# print(4-2)
+# print(3*4)
+# print(15/3)
+# print(11 % 4)
+# print(11 // 4)
+# print(2**3)
+
+
+#operadores en cadena
+# print("hola" + "mundo")
+# print("hola"*3)
+# print("hola" + "mundo"*3)
+
+#operadores de comparacion
+# print("hola" == "Hola") #Igual que
+# print("Hola" != "hola") #Distinto que
+# print(3 > 11)           #Mayor que
+# print(11 >= 10)         #Mayor igual que
+# print(10 <= 10)         #Menor igual que
+
+#Operdores de existencia
+# print("ola" in "hola")
+# print("ola" not in "hola")
+
+#operadores booleandos
+# print(True or False) #OR verifica que se cumpla una de las dos condiciones
+# print(True and False) #AND verifica que se cumpla las dos condiciones
+
+#Operadores de asignacion
+# x=1
+# x+=2
+# x*=3
+# x%=4
+# print(x)
+
+x = 6*5+8/2**4
+print(x)
