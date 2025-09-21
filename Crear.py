72949139178a47348749e29ca8751ca606d4428c
CDCR94 <david.chanr94@outlook.com>
2025-09-20 18:21:28 -0600

    Delete Crear.py

--- Crear.py
+++
@@ -1,8 +0,0 @@
-# Función para crear una lista pidiendo datos al usuario
-def crear_lista(nombre_lista):
-    lista = []
-    longitud = int(input(f"Ingrese la longitud de {nombre_lista}: "))
-    for i in range(longitud):
-        elemento = input(f"Ingrese el elemento {i+1} de {nombre_lista}: ").title()
-        lista.append(elemento)
-    return lista
