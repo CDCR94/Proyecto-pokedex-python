0e5491a38ec73e516ac2dec785c51fc026f59018
CDCR94 <david.chanr94@outlook.com>
2025-09-20 18:26:55 -0600

    Delete imprimir.py

--- imprimir.py
+++
@@ -1,5 +0,0 @@
-# Función para imprimir listas con mensaje
-def imprimir_lista(lista, mensaje):
-    print(f"\n{mensaje}:")
-    for i, elem in enumerate(lista, start=1):
-        print(f"{i}. {elem}")
