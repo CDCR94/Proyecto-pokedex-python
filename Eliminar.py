fe15aa29f865812fec572eebe3a68aab624c9a87
CDCR94 <david.chanr94@outlook.com>
2025-09-20 18:23:10 -0600

    Delete Eliminar.py

--- Eliminar.py
+++
@@ -1,4 +0,0 @@
-# Elimina elementos de lista1 que estén en lista2
-def eliminar_elementos(lista1, lista2):
-    # Usamos comprensión de listas para eliminar elementos
-    return [elemento for elemento in lista1 if elemento not in lista2]
